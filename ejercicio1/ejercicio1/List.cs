b0700e2cfb6b1c1ccda3dbd3e3343d87090aea9a
nat-78 <53804974+nat-78@users.noreply.github.com>
2020-03-25 00:32:33 -0300

    creacion terminada

---
+++ ejercicio1/ejercicio1/List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    internal class List<T1, T2>
+    {
+        internal void Add(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Dictionary<string, int> values = new Dictionary<string, int>();
+            //Rellenar diccionario con add 
+
+            values.Add("Juan", 55423412);
+            values.Add("Ernesto", 56985623);
+            values.Add("Mariana", 54787451);
+
+            // recorrer diccionario
+
+            foreach (KeyValuePair<string, int> datos in values) {
+
+                Console.WriteLine("Nombre: " + datos.Key + " " + "valor " + datos.Value);
+
+
+
+            }
+
+            Console.Read();
+        }
+    }
+}
